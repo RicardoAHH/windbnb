2892480cbf24e4deb1b815f521d30236cfc05adc
Ricardo Hernandez <richhquim@gmail.com>
2025-05-14 23:01:55 -0600

    filtrofunciona

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ import { mostrarTarjetas } from './utils.js';
 import { stays } from './stays.js';
 mostrarTarjetas(stays);
 
-let menu = document.querySelector("#menu");
+
 let btlocation = document.querySelector("#location");
 let btClose = document.querySelector("#cerrar");
 let modal = document.querySelector("#modal");
@@ -20,9 +20,16 @@ let adultcont = document.querySelector("#contAdult");
 let btadultminus = document.querySelector("#adultMinus");
 let btchildplus = document.querySelector("#childPlus");
 let childcont = document.querySelector("#contChild");
+let btchildminus = document.querySelector("#childMinus");
 let contguests = document.querySelector("#contGuests");
+let btsearch = document.querySelector("#search")
+let inputLocation = document.querySelector("#inputlocation");
+let sublocations = document.querySelector("#subLocations");
 contguests.value = 0;
 childcont.value = 0;
+localStorage.setItem("suma", 0)
+localStorage.setItem("city", "")
+localStorage.setItem("country", "")
 
 //botones para despelgar modales y cerrarlos
 btGuests.addEventListener("click", function () {
@@ -66,8 +73,6 @@ btchildplus.addEventListener(("click"), function () {
     localStorage.setItem("suma", contguests.value)
 });
 
-
-let btchildminus = document.querySelector("#childMinus");
 btchildminus.addEventListener(("click"), function () {
     if (parseInt(childcont.value) > 0) {
         childcont.value = parseInt(childcont.value) - 1;
@@ -76,24 +81,61 @@ btchildminus.addEventListener(("click"), function () {
     }
 });
 
-let btsearch = document.querySelector("#search")
-
+//Boton para buscar filtrando 
 btsearch.addEventListener(("click"), function () {
     let huespedes = localStorage.getItem("suma")
-    let filtrado = stays.filter((stay) => stay.maxGuests >= huespedes)
-    mostrarTarjetas(filtrado)
+    let ciudad = localStorage.getItem("city")
+
+    if (huespedes != 0 && ciudad != "") {
+        let filtrado = stays.filter((stay) => stay.maxGuests >= huespedes && stay.city == ciudad || stay.city == inputLocation.value)
+        mostrarTarjetas(filtrado)
+        modal.classList.toggle("hidden");
+    } else if (huespedes == 0 && ciudad != "") {
+        let filtrado = stays.filter((stay) => stay.city == ciudad || stay.city == inputLocation.value)
+        mostrarTarjetas(filtrado)
+        modal.classList.toggle("hidden");
+    } else if (huespedes != 0 && ciudad == "") {
+        let filtrado = stays.filter((stay) => stay.maxGuests >= huespedes)
+        mostrarTarjetas(filtrado)
+        modal.classList.toggle("hidden");
+    } else if (huespedes == 0 && ciudad == "") {
+        modal.classList.toggle("hidden");
+        mostrarTarjetas(stays)
+    }
 })
 
+//Boton para las locaciones 
 
-let inputLocation = document.querySelector("#inputlocation");
-let sublocations = document.querySelector("#subLocations");
 inputLocation.addEventListener("input", function () {
-    let inputValue = inputLocation.value;
-    sublocations.innerHTML = `  <div class="flex items-center gap-3">
-                        <img class="w-[18px] h-[18px]" src="./src/images/icons/location-pointer.png" alt="location">
-                        <p id="sugerencias" class="font-semibold text-[13px]">${inputValue}</p>
-                    </div>`;
-});
+    if (inputLocation.value == "") {
+        sublocations.innerHTML = "";
+        localStorage.setItem("city", "")
+        localStorage.setItem("country", "")
+    } else {
+        const entrada = inputLocation.value.toLowerCase();
+        stays.forEach((stay) => {
+            if (stay.city.toLowerCase().startsWith(entrada)) {
+                sublocations.innerHTML = `
+                        <div class="flex items-center gap-3">
+                            <img class="w-[18px] h-[18px]" src="./src/images/icons/location-pointer.png" alt="location">
+                            <p id="sugerencias" class="font-semibold text-[13px]">${stay.city},${stay.country}</p>
+                        </div>`;
+                localStorage.setItem("city", stay.city)
+                localStorage.setItem("country", stay.country)
+            }
+
+        })
+
+    }
+})
+
+//boton para copiar la sugerencia de locacion al input 
+sublocations.addEventListener("click", function () {
+    let ciudad = localStorage.getItem("city")
+    let pais = localStorage.getItem("country")
+    inputLocation.value = `${ciudad},${pais}`
+})
+
 
 
 
